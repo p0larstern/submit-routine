e18c6ba68ceb2669071ac499a0bcecb3f43d8810
p0larstern <41905327+p0larstern@users.noreply.github.com>
2018-11-25 14:22:49 +0530

    sampling code to work with users.json #2

--- user.py
+++ user.py
@@ -1,13 +1,29 @@
 import json
-users = None
 
-def openFile(mode):
-    with open("users.json", mode) as userFile:
-        global users
-        users = json.load(userFile)
+class _users_(object):
+
+    def __init__(self,mode):
+        self.mode = mode
+        self.users = None
+        self.userFile = None
+
+    def openFile(self):
+        self.userFile = open("users.json",self.mode)
+        self.users = json.load(self.userFile)
+
+    def addUser(self):
+        uname = input()
+        email = input()
+        new = {"username": uname,"email": email}
+        json.dump(new, self.userFile)
+    
+    def showUsers(self):
+        for user in self.users:
+            for param in user:
+                print("%s : %s" % (param,user[param]))
 
 if __name__ == "__main__":
-    openFile("r")
-    for user in users:
-        for param in user:
-            print("%s : %s" % (param,user[param]))
+    current = _users_("r")
+    current.openFile()
+    #current.addUser()
+    current.showUsers()
