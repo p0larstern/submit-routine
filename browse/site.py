7e15392b0e7f05e75b11eda091045c1b52df8be3
p0larstern <41905327+p0larstern@users.noreply.github.com>
2018-11-30 13:34:47 +0530

    initiating python package format

--- browse/site.py
+++ browse/site.py
@@ -2,12 +2,6 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.firefox.options import Options
 from time import sleep 
-import user
-
-current = user._users_("r")
-current.openFile()
-uname = current.userDetail("username")
-current.closeFile()
 
 options = Options()
 options.headless = True
